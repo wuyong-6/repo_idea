478ecf42ec4c8845e177f6922d525838431549ec
wuyong-6 <67037847+wuyong-6@users.noreply.github.com>
2021-11-16 21:05:44 +0800

    Update UserMapper.java

--- wy_edu_home_parent/ssm-dao/src/main/java/com/wuy/dao/UserMapper.java
+++ wy_edu_home_parent/ssm-dao/src/main/java/com/wuy/dao/UserMapper.java
@@ -57,4 +57,14 @@ public interface UserMapper {
     
     public void test11();
     public void test12();
+    
+    
+    
+    
+    public void test3();
+    public void test4(); 
+    public void test5();
+    public void test6();
+    public void test7();
+    public void test8();
 }
