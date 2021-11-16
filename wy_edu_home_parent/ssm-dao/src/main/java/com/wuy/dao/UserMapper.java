591998515f5dc40170d47401bbbff90e2b96416f
wuy <1061685930@qq.com>
2021-11-16 21:06:25 +0800

    test

--- wy_edu_home_parent/ssm-dao/src/main/java/com/wuy/dao/UserMapper.java
+++ wy_edu_home_parent/ssm-dao/src/main/java/com/wuy/dao/UserMapper.java
@@ -60,4 +60,13 @@ public interface UserMapper {
     
     public void test11();
     public void test12();
+
+    public void test13();
+    public void test14();
+    public void test15();
+    public void test16();
+    public void test17();
+    public void test18();
+
+
 }
